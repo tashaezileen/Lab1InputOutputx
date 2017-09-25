77e20cd97a551216c6b5e74d61463b12a0757baf
Nur Natasha Ezileen <tashaezileen@gmail.com>
2017-09-25 09:59:24 +0800

    test

---
+++ app/src/main/java/com/example/nurnatashaezileen/lab1inputoutput/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.nurnatashaezileen.lab1inputoutput;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvMessage;
+    Button submitButton;
+    int counter=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMessage = (TextView) findViewById (R.id.textView);
+        submitButton = (Button) findViewById (R.id.button);
+
+
+    }
+
+    public void displayText (View view){
+
+        tvMessage.setText("Welcome! - " + counter++);
+
+        //show toast
+        Toast toast = Toast.makeText(this,"Welcome", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
